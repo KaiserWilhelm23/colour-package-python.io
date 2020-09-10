6d16028831dc06735141fce01161998bd97fbeaf
blaze005 <67875237+blaze005@users.noreply.github.com>
2020-09-10 18:53:51 +0100

    Add files via upload

---
+++ color.py
@@ -0,0 +1,30 @@
+import os
+
+os.system("")
+
+
+class s():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+class eu():
+    
+    B = '\033[30m'
+    R = '\033[31m'
+    G = '\033[32m'
+    Y = '\033[33m'
+    b = '\033[34m'
+    M = '\033[35m'
+    C = '\033[36m'
+    W = '\033[37m'
+    U = '\033[4m'
+    R = '\033[0m'
+
---
+++ example.py
@@ -0,0 +1,7 @@
+from color import *
+
+
+print(s.YELLOW+'Hello ',s.RED+'World!!')
+
+print(eu.Y+'Hello ',eu.b+'World')
+
